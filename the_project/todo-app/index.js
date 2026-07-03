a904496055f1b8745cc5f7569c483fad3ca209ca
Cristian Marroquin <ccmarroquins@udistrital.edu.co>
2026-07-02 23:48:50 -0500

    Exercise 1.8: expose Todo application using Ingress

--- the_project/todo-app/index.js
+++ the_project/todo-app/index.js
@@ -1,21 +1,42 @@
-const http = require('http');
+const Koa = require('koa');
+
+const app = new Koa();
 
 const PORT = process.env.PORT || 8080;
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(`
-      <html>
-        <body>
-          <h1>Todo App</h1>
-          <p>The application is running inside Kubernetes!</p>
-        </body>
-      </html>
-    `);
-  }
+const startingString = Math.random().toString(36).substring(2, 8);
+
+console.log(`Started with ${startingString}`);
+
+app.use(async (ctx) => {
+  if (ctx.path.includes('favicon.ico')) return;
+
+  console.log(
+    `[${new Date().toISOString()}] Request received`
+  );
+
+  ctx.type = 'html';
+
+  ctx.body = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Todo App</title>
+      </head>
+      <body>
+        <h1>Todo App</h1>
+
+        <p>The application is running inside Kubernetes!</p>
+
+        <hr>
+
+        <strong>Startup ID:</strong> ${startingString}<br>
+        <strong>Timestamp:</strong> ${new Date().toISOString()}
+      </body>
+    </html>
+  `;
 });
 
-server.listen(PORT, () => {
-  console.log(`Server started in port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
